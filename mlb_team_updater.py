7c02ce9361fce78756c3ac2ab189e3f0764dc921
Thomas Cooper <thomas.cooper@rbc.com>
2018-04-07 15:01:22 -0400

    use mlbgame library for obtaining the info for the status text

--- mlb_team_updater.py
+++ mlb_team_updater.py
@@ -1,18 +1,38 @@
+import datetime
 import os
-import socket
 import sys
 from slack.updater import StatusUpdater
+import mlbgame
 TOKEN = os.environ.get('SLACK_TOKEN')
 EMAIL = os.environ.get('SLACK_EMAIL')
-TEAM = os.environ.get('MLB_TEAM_NAME')
+TEAM = os.environ.get('MLB_TEAM')
 
 
 def main():
+
+    today = datetime.datetime.now()
+
+    print("League\tDivision\tTeam\tW\tL\tPlace")
+    for division in mlbgame.standings().divisions:
+        league = division.name.split(" ")[0]
+        region = division.name.split(" ")[1]
+        for team in division.teams:
+            if team.team_abbrev == TEAM:
+                found_division = division
+                found_team = team
+            print("{}\t{}\t{}\t{}\t{}\t{}".format(
+                league, region, team.team_abbrev, team.w, team.l, team.place
+            ))
+
+    # TODO check todays games, find matching team and display either "v. <away team>" or "@<home team>"
+    todays_games = mlbgame.day(today.year, today.month, today.day)
+
     if not TOKEN or not EMAIL:
         print("Missing required environment variables. Exiting...")
         sys.exit(1)
     updater = StatusUpdater(token=TOKEN, email=EMAIL)
-    updater.update_status(status="Test status update from {}".format(socket.gethostname()))
+    updater.update_status(status="{} | {}W - {}L | #{} in {}".format("@TEX", found_team.w, found_team.l,
+                                                                     found_team.place, found_division.name))
     print("Status of user {0}: {1}".format(EMAIL, updater.display_status()))
     sys.exit(0)
 
