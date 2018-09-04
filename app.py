fabfe6be4077b1be8b80896d6187efca04ff8f48
Tom Cooper <thomascooperca@gmail.com>
2018-09-04 00:50:49 -0400

    basic stuff, add-to-slack button, etc.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return app.send_static_file("slack_button.html")
 
 @app.route("/authorize")
 def authorize():
+    #TODO Cache this user's auth code, generate a token and send message in Slack to confirm
     return "Authorized"
