6509d87b618faf39d880a1f668fbcaf05c1f8a58
Tom Cooper <thomascooperca@gmail.com>
2018-09-05 23:03:55 -0400

    couple basic tests for slack.User

--- tests/test_slack_user.py
+++ tests/test_slack_user.py
@@ -1,5 +1,18 @@
+from unittest.mock import MagicMock
 from slack.user import User
+
+reusableUser = User(token='blah', id='UB00123')
 
 def test_init():
     u = User(token='gooblygook', id='ABC123')
     assert u.id == 'ABC123'
+
+def test_status_calls_updater():
+    reusableUser.su.display_status = MagicMock(return_value="Test status")
+    reusableUser.status()
+    reusableUser.su.display_status.assert_called_with()
+
+def test_emoji_calls_updater():
+    reusableUser.su.display_status_emot = MagicMock(return_value=":cat:")
+    reusableUser.emoji()
+    reusableUser.su.display_status_emot.assert_called_with()
