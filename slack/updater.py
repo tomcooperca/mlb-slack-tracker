77f9c6141f8b181533d78e533dfd182d98baafa2
Thomas Cooper <thomascooperca@gmail.com>
2018-09-16 15:13:52 -0400

    set X-Slack-User header, default expiration of 1 week on status

--- slack/updater.py
+++ slack/updater.py
@@ -3,7 +3,7 @@
 import requests
 import json
 from urllib.parse import urlencode
-
+from datetime import datetime, timezone, timedelta
 
 class StatusUpdater:
 
@@ -16,21 +16,28 @@ class StatusUpdater:
             requests.packages.urllib3.disable_warnings()
         self.default_headers = {
             'Authorization': 'Bearer {}'.format(self.token),
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-Slack-User': id
         }
 
 
     def update_status(self, status=None):
+        print("Updating status of {} to \"{}\"".format(self.id, status))
         current_emot = self.display_status_emot()
+        today_utc = datetime.now().replace(tzinfo=timezone.utc)
+        next_week_timestamp = int((today + timedelta(days=7)).timestamp())
         update = {
-            'user': self.id,
             'profile': {
                 'status_text': status if status else self.display_status(),
-                'status_emoji': current_emot
+                'status_emoji': current_emot,
+                'status_expiration': '{}'.format(next_week_timestamp)
             }
         }
-        requests.post('{}/users.profile.set'.format(self.slack_url), data=json.dumps(update),
+        response = requests.post('{}/users.profile.set'.format(self.slack_url), data=json.dumps(update),
                                  headers=self.default_headers, verify=self.ssl_verify)
+        print(response.status_code)
+        print(response.text)
+
 
     def display_status_emot(self):
         return self.get_profile()['profile']['status_emoji']
