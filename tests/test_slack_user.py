a42e2e16a8194643c894678181f1f56b0248b9a2
Thomas Cooper <thomascooperca@gmail.com>
2018-09-17 00:38:58 -0400

    fix test, clean it up too

--- tests/test_slack_user.py
+++ tests/test_slack_user.py
@@ -12,21 +12,38 @@ def test_init():
     u = User(token='gooblygook', id='ABC123', team=None)
     assert u.id == 'ABC123'
 
+
 def test_status_calls_updater():
     reusableUser.su.display_status = MagicMock(return_value="Test status")
     reusableUser.status()
     reusableUser.su.display_status.assert_called_with()
+
 
 def test_emoji_calls_updater():
     reusableUser.su.display_status_emot = MagicMock(return_value=":cat:")
     reusableUser.emoji()
     reusableUser.su.display_status_emot.assert_called_with()
 
-def test_simple_team_and_standings_status():
-    expected = 'City Name Players | 0W-162L | CL Beast'
+
+def test_simple_team_and_record_status():
+    expected = 'CN | 0W-162L'
     u = User(token='blah', id='UB00123', team=testTeam)
     u.su.update_status = MagicMock()
+    u.simple_team_and_record()
+    u.su.update_status.assert_called_once_with(status=expected)
 
-    u.simple_team_and_standings()
 
+def test_todays_game_and_standings_status():
+    expected = 'CN@BOB | 0W-162L | #5 in CL Beast'
+    u = User(token='blah', id='UB00123', team=testTeam)
+    u.su.update_status = MagicMock()
+    u.todays_game_and_standings()
+    u.su.update_status.assert_called_once_with(status=expected)
+
+
+def test_todays_game_and_standings_status():
+    expected = 'CN@BOB | 0W-162L | #5 in CL Beast'
+    u = User(token='blah', id='UB00123', team=testTeam)
+    u.su.update_status = MagicMock()
+    u.todays_game_and_standings()
     u.su.update_status.assert_called_once_with(status=expected)
